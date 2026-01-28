e2e7dfd5efa2ab0b81b49494b2cdb86abe3dfab5
razzat008 <93041325+razzat008@users.noreply.github.com>
2026-01-28 07:31:54 +0545

    updated: added error checks and lowercase

--- useit.sh
+++ useit.sh
@@ -1,12 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-ignore=$1
-echo $ignore
-
-if [[ $# -eq 0 ]]; then
-  fetch $ignore
-fi
+ignore=$1 # first argument to the script
+ignore=${ignore,,} # making things small
+file="./gitignores/$ignore.gitignore"
 
 fetch() {
-  echo $1
+  echo "You're generating .gitignore for: $ignore"
+  get_ignores "$1" >> test.txt
 }
+
+get_ignores() {
+  cat $1
+}
+
+if [[ $# -eq 0 ]]; then
+  echo "Usage: $0 <language>"
+  exit 1
+fi
+
+if [[ -f "$file" ]]; then
+  fetch "$file"
+else
+  echo "Couldn't find the specified .gitignore."
+  exit 1
+fi
